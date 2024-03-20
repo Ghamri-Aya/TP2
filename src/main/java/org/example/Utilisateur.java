7e9769ff7f509f9903335ebc7086fa6d5b7b0f52
ghamri <ayaghamri07@gmail.com>
2024-03-20 10:38:50 +0100

    Initial commit

---
+++ src/main/java/org/example/Utilisateur.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Utilisateur {
+    private String nom;
+    private String prenom;
+    private String email;
+
+    public Utilisateur(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+}
